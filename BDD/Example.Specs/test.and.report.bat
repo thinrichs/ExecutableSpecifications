5d2ca59fe199a15591d0a4989383c9f22b88c218
Tommy Hinrichs <tommy.hinrichs@gmail.com>
2015-03-17 23:33:01 -0600

    fix dll/ package versioning

--- BDD/Example.Specs/test.and.report.bat
+++ BDD/Example.Specs/test.and.report.bat
@@ -7,5 +7,5 @@ set outputPath=%binPath%testreports\%testSet%\
 
 @echo on
 mkdir %outputPath%
-%packagesPath%NUnit.Runners.2.6.4\tools\nunit-console /labels /out=%outputPath%TestResult.txt /xml=%outputPath%TestResult.xml %binPath%Example.Specs.dll /include:%testSet% /apartment=sta
+%packagesPath%NUnit.Runners.2.6.3\tools\nunit-console /labels /out=%outputPath%TestResult.txt /xml=%outputPath%TestResult.xml %binPath%Example.Specs.dll /include:%testSet% /apartment=sta
 %packagesPath%SpecFlow.1.9.0\tools\specflow.exe nunitexecutionreport ..\Example.Specs.csproj /testOutput:%outputPath%TestResult.txt /xmlTestResult:%outputPath%TestResult.xml /out:%outputPath%%testSet%-TestResult.html
