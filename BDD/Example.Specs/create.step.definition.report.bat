e5b6b8bd9531a4f204cc684206a0c0816ff19fac
Tommy Hinrichs <tommy.hinrichs@gmail.com>
2015-03-21 12:28:53 -0600

    Fix some pathing

--- BDD/Example.Specs/create.step.definition.report.bat
+++ BDD/Example.Specs/create.step.definition.report.bat
@@ -1,2 +1,2 @@
-mkdir bin\testreports
-..\packages\SpecFlow.1.9.0\tools\specflow.exe stepdefinitionreport Example.Specs.csproj /binFolder:bin /out:bin\testreports\StepDefinitionReport.html
+mkdir %~dp0bin\testreports
+%~dp0..\packages\SpecFlow.1.9.0\tools\specflow.exe stepdefinitionreport %~dp0\Example.Specs.csproj /binFolder:%~dp0bin /out:%~dp0bin\testreports\StepDefinitionReport.html
